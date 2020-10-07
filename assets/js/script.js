4251a73da4ad200634da6cde8e91664c7e9890f2
Alfonso Alba <alfonso@alfonsoalba.com>
2020-10-07 22:53:37 +0200

    Añade imagen a la cabecera de la página web.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,5 @@
     Custom scripts ( Any custom script you want to apply should be defined here ).
 ====================================================================================*/
 
+    // Landing page banner
+    $(function() { $(".git-landing-typed").typed({ strings: ["Trabaja con", "Utiliza", "Comprende", ], typeSpeed: 130, loop: true }); });
